bf820d74ed9dd3b37ffe128d7996d0a022a3acc8
Weng Ti <wengti@hotmail.com>
2026-05-24 15:57:53 +0800

    added a toggle to filter by this month only

--- frontend/components/content/CustomersHome.tsx
+++ frontend/components/content/CustomersHome.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { Database } from "@/types/supabase"
-import { useState } from "react"
+import React, { useState } from "react"
+import { Toggle } from "../tailgrids/core/toggle"
 
 export type CustomerType = Database["public"]["Tables"]["customers"]["Row"]
 
@@ -9,12 +10,24 @@ type CustomersHomePropsType = {
     customersData: CustomerType[]
 }
 
-export default function CustomersHome({customersData}: CustomersHomePropsType){
+export default function CustomersHome({ customersData }: CustomersHomePropsType) {
 
     const [isFiltered, setIsFiltered] = useState<boolean>(false)
     const [displayedCustomer, setDisplayedCustomer] = useState<CustomerType[]>(customersData)
 
+    function handleFilter(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsFiltered(event.target.checked)
+    }
+
     return (
-        <></>
+        <>
+            <div className='ml-auto mr-4 my-4'>
+                <Toggle
+                    label="Show contracts that expire this month"
+                    checked={isFiltered}
+                    onChange={handleFilter}
+                />
+            </div>
+        </>
     )
 }
