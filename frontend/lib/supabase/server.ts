de2fe104bcb2d312f47e235710e2f9fb6a020073
Weng Ti <wengti@hotmail.com>
2026-05-24 15:34:12 +0800

    generated and used type schema from supabase

--- frontend/lib/supabase/server.ts
+++ frontend/lib/supabase/server.ts
@@ -1,3 +1,4 @@
+import { Database } from "@/types/supabase";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
@@ -9,7 +10,7 @@ import { cookies } from "next/headers";
 export async function createClient() {
   const cookieStore = await cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
