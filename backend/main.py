325ae6b371d32f4fb513b6fd3be7b2ed1d464bc7
Weng Ti <wengti@hotmail.com>
2026-05-25 09:53:09 +0800

    created the template_dict required for llm

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,8 @@ from data.db_client import create_supabase_client
 from model.model import CustomerDataModel, PitcherPromptModel
 from postgrest.exceptions import APIError
 
+from model.supabase_model import PublicPlans
+
 load_dotenv(override=True)
 
 app = FastAPI()
@@ -43,6 +45,7 @@ def generate_pitch(customer_data: CustomerDataModel):
         else:
             new_plan_name = "Tier 1"
 
+        # Fetch plan details
         supabase = create_supabase_client()
         response = (
             supabase.table("plans")
@@ -55,9 +58,9 @@ def generate_pitch(customer_data: CustomerDataModel):
         new_plan_data = None
         for data in response["data"]:
             if data["name"] == cur_plan_name:
-                cur_plan_data = data
+                cur_plan_data = PublicPlans(data)
             elif data["name"] == new_plan_name:
-                new_plan_data = data
+                new_plan_data = PublicPlans(data)
 
         if cur_plan_data == None or new_plan_data == None:
             raise HTTPException(
@@ -66,6 +69,33 @@ def generate_pitch(customer_data: CustomerDataModel):
             )
 
         # Convert data into template dict
+        pitcher_template_dict = PitcherPromptModel(
+            customer_name=customer_data.name,
+            tenure_start=customer_data.tenure_start,
+            tenure_end=customer_data.tenure_end,
+            monthly_usage=str(customer_data.monthly_usage),
+            cur_plan_name=customer_data.plan,
+            cur_plan_price=str(cur_plan_data.plan_price),
+            cur_download_speed=str(cur_plan_data.download_speed),
+            cur_upload_speed=str(cur_plan_data.upload_speed),
+            new_plan_name=new_plan_data.name,
+            new_plan_price=str(new_plan_data.plan_price),
+            new_download_speed=str(new_plan_data.download_speed),
+            new_upload_speed=str(new_plan_data.upload_speed),
+            new_plan_duration_months=str(new_plan_data.plan_duration_months),
+            router=new_plan_data.router,
+            mesh_price=f"RM {new_plan_data.mesh_price} per month",
+            fttr_price=(
+                f"RM {new_plan_data.fttr_price} per month"
+                if new_plan_data.fttr_price is not None
+                else None
+            ),
+            promotion=(
+                f"RM {new_plan_data.plan_price_promo} for the first 6 months"
+                if new_plan_data.plan_price_promo is not None
+                else None
+            ),
+        ).model_dump()
 
         # Invoke the LLM chain
 
